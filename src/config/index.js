182a92f5bf971847b3087760bb765bddd9b54d67
Ifaldzi <ifaldzialwihudhori@gmail.com>
2022-12-23 15:18:08 +0700

    feat: add sequelize configuration

---
+++ src/config/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+
+const config = {
+  database: {
+    database: process.env.DB_NAME,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: process.env.DB_DIALECT,
+    schema: process.env.DB_SCHEMA,
+  }
+}
+
+export default config
